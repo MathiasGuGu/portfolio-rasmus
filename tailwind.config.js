d0b24b5472885c39881a806189e254ff0e040c63
sindrebenjamin <sindskog@gmail.com>
2024-01-15 13:49:30 +0100

    configure tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Montserrat', 'sans-serif'],
+				serif: ['Montaga', 'serif']
+			}
+		}
 	},
 	plugins: []
 };
