ef144d871c1610619c5788ba5de6f9e4fda49f01
sindrebenjamin <sindskog@gmail.com>
2024-01-17 14:20:16 +0100

    add content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,8 @@
     import NextButton from "../lib/components/NextButton.svelte";
 </script>
 
-<NextButton href="/om">Om meg</NextButton>
+<div class="p-5">
+    <h1 class="font-serif">Mitt navn er Rasmus Nilsen</h1>
+    <img src="/assets/Ellipse_1.png" alt="Rasmus smiler mens han holder armen sin opp mot hodet">
+    <NextButton href="/om">Om meg</NextButton>
+</div>
