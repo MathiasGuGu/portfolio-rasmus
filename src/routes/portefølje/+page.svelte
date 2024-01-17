270a5869f97ce9274bb81c31b068a0ae1b7adc41
sindrebenjamin <sindskog@gmail.com>
2024-01-17 11:08:28 +0100

    make aethetic changes to portfolio page and add all images to data.js

--- src/routes/portefølje/+page.svelte
+++ src/routes/portefølje/+page.svelte
@@ -7,11 +7,11 @@
 {#each data.summaries as {title, subtitle, slug, cover}, i}
 
 <div class="items-center md:grid md:grid-cols-2">
-<figure class="opacity-50 hidden md:block text-8xl {(i + 1) % 2 === 0 ? "order-2 text-right" : "order-1"}">
+<figure class="peer-hover:opacity-100 select-none opacity-50 hidden md:block text-8xl {(i + 1) % 2 === 0 ? "order-2 text-right" : "order-1"}">
    {i+1}
 </figure>
-<a class="{(i + 1) % 2 === 0 ? "order-1" : "order-2"} hover:opacity-50 transition-all" href="/portefølje/{slug}">
-    <h2 class="text-2xl font-bold whitespace-nowrap">{title}</h2>
+<a class="{(i + 1) % 2 === 0 ? "order-1" : "order-2"} peer hover:opacity-50 transition-all" href="/portefølje/{slug}">
+    <h2 class="text-2xl md:text-3xl font-bold whitespace-nowrap">{title}</h2>
     <h3 class="mb-4">{subtitle}</h3>
     <img class="aspect-square object-cover w-full" src={cover} alt="{title} - {subtitle}">
 </a>
